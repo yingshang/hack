bbdc22a1925ea3aecea63b180393b1af7a58964e
Leon <wushangleon@gmail.com>
2017-06-03 14:57:41 +0800

    sdsa

---
+++ hack/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for hack.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', hack.views.home, name='home'),
+    # url(r'^hack/', include('hack.hack.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
